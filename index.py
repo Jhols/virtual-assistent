f61e1ec1e01863dfba91053fb56b5162c1054ca0
João Pedro Hegouet <jphegouet@gmail.com>
2025-02-25 18:00:27 -0300

    feat: inserted engine voice to speak the answers

--- index.py
+++ index.py
@@ -1,12 +1,22 @@
 import speech_recognition as sr
 import re
+import pyttsx3
 
 name = ''
+
+def engineAnswer(answer):
+    engine.say(answer)
+    engine.runAndWait()
+    print(answer)
 
 while(True):
     mic = sr.Recognizer()
 
     with sr.Microphone() as source:
+        engine = pyttsx3.init()
+        
+        # engine.setProperty('voice', 'com.apple.speech.synthesis.voice.luciana') # for portuguese
+
         mic.adjust_for_ambient_noise(source)
 
         print("Let's start! Speak something...")
@@ -19,26 +29,31 @@ while(True):
             print("You said: " + phrase)
 
             if (re.search(r'\b' + "bye-bye" + r'\b', format(phrase))):
-                print('See you soon!')
+                engineAnswer('See you soon!')
                 break
 
+            elif (re.search(r'\b' + "hello" + r'\b', format(phrase))):
+                engineAnswer('Hi there!')
+
             elif (re.search(r'\b' + "help" + r'\b', format(phrase))):
-                print('Something related to helping.')
+                engineAnswer('Something related to helping.')
 
             elif (re.search(r'\b' + "my name is" + r'\b', format(phrase))):
                 t = re.search("my name is (.*)", format(phrase))
                 if t:
                     name = t.group(1)
-                    print("Your name is " + name)
+                    engineAnswer("Your name is " + name)
+
 
             elif (re.search(r'\b' + "what is my name" + r'\b', format(phrase))):
                 if name:
-                    print("Your name is " + name)
+                   engineAnswer("Your name is " + name)
+
                 else:
-                    print("I don't know your name. Please, tell me.")
+                    engineAnswer("I don't know your name. Please, tell me.")
 
             elif (re.search(r'\b' + "thank you" + r'\b', format(phrase))):
-                print("You're welcome!")
+                engineAnswer("You're welcome!")
 
         except sr.UnknownValueError:
             print("oops... something goes wrong.")
